2d00f722bb49b9edf8b105a60f5bb04d65893b9f
sasanzare <sasanzare75@gmail.com>
2022-06-26 12:05:57 +0430

    Initial commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by sasan on 6/26/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
